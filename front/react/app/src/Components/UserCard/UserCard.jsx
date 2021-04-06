67c3ca064cb4626c393e813aec33b76e80d283f0
Arthur <arthur001123@gmail.com>
2021-04-06 11:37:43 -0300

    making request to get user data on react front

--- front/react/app/src/Components/UserCard/UserCard.jsx
+++ front/react/app/src/Components/UserCard/UserCard.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
 import "./UserCard.css";
 
+import axios from "axios";
+
 class UserCard extends Component {
+  state = {
+    user: {},
+  };
+
+  componentDidMount() {
+    axios.get("http://127.0.0.1:1212/getuser").then((res) => {
+      const user = res.data;
+      console.log(res.data);
+      this.setState({ user });
+      console.log(this.state);
+    });
+  }
+
   render() {
     return (
       <div className="userCardDiv">
         <div className="cardBar">
-          <h1 className="userName"> Full User Name </h1>
+          <h1 className="userName"> {this.state.user.name} </h1>
         </div>
         <div className="cardBody">
-          <div className="cardInfo">Main Role: User Main Role</div>
-          <div className="cardInfo">Github: User Github</div>
-          <div className="cardInfo">LinkedIn: User LinkedIn</div>
+          <div className="cardInfo">Main Role: {this.state.user.mainRole}</div>
+          <div className="cardInfo">Github: {this.state.user.gitHub}</div>
+          <div className="cardInfo">LinkedIn: {this.state.user.linkedIn}</div>
         </div>
       </div>
     );
